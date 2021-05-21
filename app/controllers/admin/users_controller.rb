be1040fdbb85e50f9667aacb80328f64b9ba0e6e
Nat_Lin <not052683@gmail.com>
2021-05-21 16:47:41 +0800

    add admin show user tasks

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,11 @@
 class Admin::UsersController < ApplicationController
 
   def index
-    @users = User.all
+    @users = User.includes('tasks').page(params[:page])
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    @tasks = @user.tasks.page(params[:page])
   end
 end
